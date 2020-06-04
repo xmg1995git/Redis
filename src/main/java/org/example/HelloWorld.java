ed2dac963a5874d997977e7d51bd95ea4873231a
xmg1995git <xmg1995@aliyun.com>
2020-06-04 11:01:27 +0800

    hello 打招呼

---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @author xiaoMaGe
+ * @date 2020-06-04 - 10:57
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello Redis");
+    }
+}
